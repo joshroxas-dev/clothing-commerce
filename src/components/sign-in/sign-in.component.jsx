56377e36db0bf16082b6d9c7649150d40da429be
joshroxas-dev <joshuabroxas@gmail.com>
2021-06-28 06:19:23 -0800

    added authentication component

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import "./sign-in.styles.scss";
+
+class SignIn extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+            email: "",
+            password: ""
+        };
+	}
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        this.setState({ email: "", password: ""});
+    }
+
+    handleChange = (event) => {
+        const {value, name} = event.target;
+
+        this.setState({[name]: value});
+    }
+
+	render() {
+		return (
+			<div className="sign-in">
+                <h2>I already have an account.</h2>
+                <span>Sign in with your email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <input type="email" name="email" value={this.state.email} required onChange={this.handleChange} />
+                    <label htmlFor="email">Email</label>
+                    <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
+                    <label htmlFor="password">Password</label>
+
+                    <input type="submit" value="Sign In" />
+                </form>
+			</div>
+		);
+	}
+}
+
+export default SignIn;
---
+++ src/pages/authentication/authentication.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SignIn from "../../components/sign-in/sign-in.component";
+import "./authentication.styles.scss";
+
+const AuthenticationPage = () => {
+	return (
+        <div className="sign-in-and-sign-up">
+            <SignIn />
+        </div>
+    )
+};
+
+export default AuthenticationPage;
