62c26fb0f327e5b164469ca3df68eed3333ff29b
joshroxas-dev <joshuabroxas@gmail.com>
2021-07-02 00:07:54 +0800

    finished authentication

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import AuthenticationPage from "./pages/authentication/authentication.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends Component {
 	constructor(props) {
@@ -19,9 +19,21 @@ class App extends Component {
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
-		this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-			this.setState({ currentUser: user });
-			console.log(user);
+		this.unsubscribeFromAuth = auth.onAuthStateChanged( async (userAuth) => {
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
+
+				userRef.onSnapshot(snapshot => {
+					this.setState({
+						currentUser: {
+							id: snapshot.id,
+							...snapshot.data()
+						}
+					})
+				})
+			} else {
+				this.setState({ currentUser: userAuth });
+			}
 		});
 	}
 
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignIn extends Component {
 	constructor(props) {
@@ -14,10 +14,17 @@ class SignIn extends Component {
 		};
 	}
 
-	handleSubmit = (event) => {
+	handleSubmit = async (event) => {
 		event.preventDefault();
 
-		this.setState({ email: "", password: "" });
+		const { email, password } = this.state;
+
+		try {
+			await auth.signInWithEmailAndPassword(email, password);
+			this.setState({ email: "", password: ""})
+		} catch (error) {
+			console.log(error);
+		}
 	};
 
 	handleChange = (event) => {
@@ -29,25 +36,25 @@ class SignIn extends Component {
 	render() {
 		return (
 			<div className="sign-in">
-				<h2>I already have an account.</h2>
+				<h2 className="title">I already have an account.</h2>
 				<span>Sign in with your email and password</span>
 
 				<form onSubmit={this.handleSubmit}>
 					<FormInput
-						type="email"
+						type="text"
 						name="email"
 						label="email"
 						value={this.state.email}
-						required
 						handleChange={this.handleChange}
+						required
 					/>
 					<FormInput
 						type="password"
 						name="password"
 						label="password"
 						value={this.state.password}
-                        required
 						handleChange={this.handleChange}
+						required
 					/>
 					<div className="buttons">
 						<CustomButton type="submit">Sign In</CustomButton>
